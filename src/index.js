1c2952fedd4d3150c3a51d475857c02aa3d02a5b
g-ka <gkaphilathithiya@gmail.com>
2023-12-10 06:08:16 +0530

    data_provider_update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Data_provider } from './context/Data_box';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router basename='/unity'>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>         
+      <Data_provider>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>  
+      </Data_provider>             
     </Router>    
   </React.StrictMode>
 );
